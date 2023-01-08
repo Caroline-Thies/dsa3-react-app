fdde19293398311629211af97f5db352dc372467
Caroline <carolinethies01@gmail.com>
2023-01-08 08:46:02 +0100

    test gh pages

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Home from "./Pages/Home";
 import { useState } from "react";
 
 function App() {
+  /*
   const [currentPage, setCurrentPage] = useState("Login");
 
   const navigate = (page) => {
     setCurrentPage(page);
   };
+
 
   const renderPage = () => {
     if (currentPage === "Login") {
@@ -17,8 +19,9 @@ function App() {
       return <Home navigate={navigate} />;
     }
   };
+  */
 
-  return <div className="App">{renderPage()}</div>;
+  return <div className="App">Test</div>;
 }
 
 export default App;
